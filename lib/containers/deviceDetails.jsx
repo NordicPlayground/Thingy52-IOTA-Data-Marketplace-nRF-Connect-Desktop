261ee918f21fb60f46366af4bb9b1672f25e5ecf
Lavrans Grønseth <lavrans.gronseth@gmail.com>
2018-03-12 14:21:15 +0100

    EUREKA! got data from the thingy

--- lib/containers/deviceDetails.jsx
+++ lib/containers/deviceDetails.jsx
@@ -13,55 +13,114 @@ import * as AdapterActions from '../actions/adapterActions';
 import { traverseItems, findSelectedItem } from './../common/treeViewKeyNavigation';
 import { getInstanceIds } from '../utils/api';
 
-
+const NOTIFY = 1;
+const INDICATE = 2;
+let buttonClicked = 0;
+const CCCD_UUID = "2902"
 
 export class DeviceDetailsContainer extends React.PureComponent {
 
     constructor(props){
         super(props)
         this.buttonClicked = this.buttonClicked.bind(this)
+        this.writeDescriptorButtonClicked = this.writeDescriptorButtonClicked.bind(this)
+        this.onToggleNotify = this.onToggleNotify.bind(this)
+        this.findCccdDescriptor = this.findCccdDescriptor.bind(this)
+        this.isNotifying = this.isNotifying.bind(this)
     }
 
     static contextTypes = {
         store: React.PropTypes.object
     }
-    componentDidMount () {
-        
 
+    findCccdDescriptor(children) {
+        if (!children) {
+            return undefined;
+        }
+
+        return children.find(child => child.uuid === CCCD_UUID);
     }
 
-    buttonClicked() {
+    isNotifying(cccdDescriptor) {
+        if (!cccdDescriptor) {
+            return false;
+        }
+    
+        const valueArray = cccdDescriptor.value.toArray();
+    
+        if (valueArray.length < 2) {
+            return false;
+        }
+    
+        return ((valueArray[0] & (NOTIFY | INDICATE)) > 0);
+    }
+
+    onToggleNotify(characteristic) {
+        const cccdDescriptor = this.findCccdDescriptor(characteristic.get("children")) //fiks hardkoding
+        const isDescriptorNotifying = this.isNotifying(cccdDescriptor);
+        const hasNotifyProperty = characteristic.properties.notify//this.props.item.properties.notify;
+        const hasIndicateProperty = characteristic.properties.indicate//this.props.item.properties.indicate;
+
+        if (cccdDescriptor === undefined) {
+            return;
+        }
+
+        if (!hasNotifyProperty && !hasIndicateProperty) {
+            return;
+        }
+
+        let cccdValue;
+        logger.error(cccdDescriptor.value);
+        if (!isDescriptorNotifying) {
+            if (hasNotifyProperty) {
+                cccdValue = NOTIFY;
+            } else {
+                cccdValue = INDICATE;
+            }
+        } else {
+            cccdValue = 0;
+        }
+
+        const value = [cccdValue, 0];
+        //this.props.onWriteDescriptor(this.cccdDescriptor, value);
+        this.context.store.dispatch(DeviceDetailsActions.writeDescriptor(cccdDescriptor,value))
+        console.log("weather3",JSON.stringify(characteristic,null,2))
+    }
+
+    writeDescriptorButtonClicked(){
         let state = this.context.store.getState()
         const deviceDetails = state.app.adapter.getIn(['adapters', state.app.adapter.selectedAdapterIndex, 'deviceDetails']);
         let deviceDetail = deviceDetails.devices.get("F8:1B:03:0B:46:5D.0");
         const children = deviceDetail.get("children")
         const weather = children.get("F8:1B:03:0B:46:5D.0.5")
-        console.log(weather)
-    
-        dispatch(DeviceDetailsActions.setAttributeExpanded(weather, !weather.expanded))
 
-        /*
-        
-        if(children){
-            children.forEach(child1 => {
-                console.log("layer 1: ", child1.uuid)
-                if (child1.get("children")){
-                    child1.get("children").forEach(child2 => {
-                        if(child2.uuid)
+        this.onToggleNotify(weather.get("children").get("F8:1B:03:0B:46:5D.0.5.6"))
+    }
 
-                    })
-                }
+    buttonClicked() {
+        if (buttonClicked === 0){
 
-            })
         }
-        */
+        let state = this.context.store.getState()
+        const deviceDetails = state.app.adapter.getIn(['adapters', state.app.adapter.selectedAdapterIndex, 'deviceDetails']);
+        let deviceDetail = deviceDetails.devices.get("F8:1B:03:0B:46:5D.0");
+        const children = deviceDetail.get("children")
+        const weather = children.get("F8:1B:03:0B:46:5D.0.5")
+        console.log(weather.get("children"))
+    
+        this.context.store.dispatch(DeviceDetailsActions.setAttributeExpanded(weather, !weather.expanded))
+        console.log("weather2: ",JSON.stringify(weather,null,2))
     }
         
 
     render() {
 
         return(
-            <div><button onClick={this.buttonClicked}>click me</button> Hello world</div>
+            <div>
+                <div><button onClick={this.buttonClicked}>expand attributes</button></div>
+                <div><button onClick={this.writeDescriptorButtonClicked}>write descriptor</button></div>
+            </div>
+            
         );
     }
 
